e88804369b049b3659254532154f432f367b1b9f
vic fir <vicfir@hotmail.fr>
2023-01-13 12:15:20 +0100

    fin

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log(document.getElementsByClassName("redPurple"));
+
+console.log(document.querySelectorAll("redPurple"));
+
+console.log(document.querySelectorAll("h1.redPurple"));
+
+console.log(document.querySelectorAll("li,p,span"));
+
+console.log(document.querySelectorAll("li.important,p"));
+
+console.log(document.querySelectorAll("h1.redPurple,span.redPurple"));
